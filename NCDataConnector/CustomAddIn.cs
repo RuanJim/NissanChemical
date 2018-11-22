86b87711111965d003338a91e6b8dbb305acc45a
Jim <jim.ruan@perkinelmer.com>
2018-11-22 10:37:43 +0800

    Update project template.

--- NCDataConnector/CustomAddIn.cs
+++ NCDataConnector/CustomAddIn.cs
@@ -1,15 +1,27 @@
-using System;
-using System.Windows.Forms;
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CustomAddIn.cs" company="PerkinElmer Inc.">
+//   Copyright (c) 2013 PerkinElmer Inc.,
+//     940 Winter Street, Waltham, MA 02451.
+//     All rights reserved.
+//     This software is the confidential and proprietary information
+//     of PerkinElmer Inc. ("Confidential Information"). You shall not
+//     disclose such Confidential Information and may not use it in any way,
+//     absent an express written license agreement between you and PerkinElmer Inc.
+//     that authorizes such use.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
 
-using Spotfire.Dxp.Application;
+#region
+
 using Spotfire.Dxp.Application.Extension;
 
-namespace NCDataConnector
+#endregion
+
+namespace Com.PerkinElmer.Service.NCDataConnector
 {
     /// <summary>
     /// </summary>
     public sealed class CustomAddIn : AddIn
     {
-        // Override methods in this class to register your extensions.
     }
 }
